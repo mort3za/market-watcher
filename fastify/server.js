aa16c9db35fbd00a21f24bb111757c9eb9b1856e
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-05-18 11:14:47 +0430

    debugging server

--- fastify/server.js
+++ fastify/server.js
@@ -39,14 +39,17 @@
 
 "use strict";
 const cors = require("cors");
-const isProduction = process.env.NODE_ENV === "production";
-const log = require("pino")({
-  level: "info",
-  prettyPrint: true,
-});
-const fastify = require("fastify")({
-  logger: !isProduction ? log : false,
-});
+// const isProduction = process.env.NODE_ENV === "production";
+// const log = require("pino")({
+//   level: "info",
+//   prettyPrint: true,
+// });
+
+// const fastifyOptions = {
+//   logger: !isProduction ? log : false,
+// };
+const fastifyOptions = {};
+const fastify = require("fastify")(fastifyOptions);
 fastify.use(cors());
 fastify.options("*", (request, reply) => {
   reply.send();
@@ -61,8 +64,12 @@ fastify.get("/", async (request, reply) => {
 
 const start = async () => {
   try {
+    console.log('before listen');
     await fastify.listen(4000);
+    console.log('after listen');
+    
   } catch (err) {
+    console.log('catch listen');
     fastify.log.error(err);
     process.exit(1);
   }
